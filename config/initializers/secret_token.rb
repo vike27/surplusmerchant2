877cc0036667861c6d5b40f6df84434de03e4ddf
Javier Noris <norisjavier@gmail.com>
2014-06-24 23:30:35 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surplusmerchant2::Application.config.secret_key_base = '1ae4d4d9b2f5a7c5bcef6b7f9181e078068cc05a38dd5c88af53b02eb4595fae653c382c4295c2b748c1f7b767118fd93389b3d507612134a269fc4e1a4d2528'
