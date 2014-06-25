4c726f44812d1f439a2b98d775e7c8196b32a6ad
Javier Noris <norisjavier@gmail.com>
2014-06-24 23:39:17 -0700

    added static home page to test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Surplusmerchant2::Application.routes.draw do
+
+    root :to => "staticpages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
