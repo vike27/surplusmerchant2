e5d214438768cce39e06839944841555ac9d5c93
Javier Noris <norisjavier@gmail.com>
2014-06-25 01:51:37 -0700

    trying to get asset pipeline for production working

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -5,10 +5,7 @@
 #
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
-Spree.config do |config|
-  # Example:
-  # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
-end
+
 
 Spree.user_class = "Spree::User"
+
