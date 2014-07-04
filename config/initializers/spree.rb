70b360394a0f5903d8d21f781872309f36440cde
Javier Noris <norisjavier@gmail.com>
2014-07-04 16:19:16 -0700

    undoing paperclip stuff

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,18 +9,5 @@
 
 Spree.user_class = "Spree::User"
 
-Spree.config do |config|
-  # Spree 2.2 moved Paperclip settings out of the admin page, set 'em here:
-  attachment_config = {
-    s3_credentials: {
-      access_key_id: ENV.fetch("AKIAJQP7OWOJ6AD6LDOQ"),
-      secret_access_key: ENV.fetch("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"),
-      bucket: ENV.fetch("surplusmerchant2bucket"),
-    }
-  }
 
-  attachment_config.each do |key, value|
-    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-  end
-end
 
