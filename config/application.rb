76632ce4436a289298eb7e497b675d6260eeb7b3
Javier Noris <norisjavier@gmail.com>
2014-07-04 11:49:01 -0700

    editing config application file to try and fix asset precompiling

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module Surplusmerchant2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    
   end
 end
+
 
